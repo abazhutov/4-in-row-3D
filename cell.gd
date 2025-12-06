e38f1400b1f16cb889018b3dfeb1bf95f3a9d0de
abazhutov <abazhutov@mail.ru>
2025-12-06 23:44:08 +0400

    improve gamplay. add shost piece

--- cell.gd
+++ cell.gd
@@ -2,6 +2,8 @@ extends Area3D
 
 # Сигнал, который будет отправлен главному контроллеру при клике
 signal cell_clicked(coords: Vector3i)
+signal cell_hovered(coords: Vector3i)
+signal cell_unhovered # Просто сигнал, без координат, для очистки
 
 # Координаты ячейки в логическом массиве [x][y][z]
 var board_coords: Vector3i
@@ -16,3 +18,12 @@ func _input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		# Если нажатие подтверждено, отправляем сигнал, передавая свои координаты
 		cell_clicked.emit(board_coords)
+		
+# Обработка наведения (используем встроенные сигналы Area3D)
+func _on_mouse_entered():
+	# Отправляем сигнал о наведении с координатами
+	cell_hovered.emit(board_coords)
+
+func _on_mouse_exited():
+	# Отправляем сигнал об уходе мыши (для удаления призрака)
+	cell_unhovered.emit()
